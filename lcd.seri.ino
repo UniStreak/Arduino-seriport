12ff1453a447939b9c4ec91db9fc08d367d75c08
Emre <37214507+UniStreak@users.noreply.github.com>
2019-02-26 00:56:23 +0300

    Add files via upload

---
+++ lcd.seri.ino
@@ -0,0 +1,56 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8,9,4,5,6,7);
+String str1="",str2="";
+boolean stringcomp=false;
+int cnt=0;
+
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("Veri bekleniyor");
+}
+void SerialEvent()
+{
+  while(Serial.available())
+  {
+    char inChar=(char)Serial.read();
+    cnt++;
+    if(inChar=='\n')
+      stringcomp=true;
+
+    if(inChar=='*')
+    {
+      lcd.clear();
+      str1="";
+      str2="";
+      stringcomp=true;
+    }
+
+    if(stringcomp==false && cnt<=16)
+      str1+=inChar;
+
+    if(stringcomp==false && cnt>16 && cnt<=32)
+      str2+=inChar;
+    }
+  }
+
+void loop() {
+  lcd.setCursor(0,0);
+  SerialEvent();
+  if(stringcomp)
+  {
+    lcd.clear();
+    lcd.print(str1);
+    lcd.setCursor(0,1);
+    lcd.print(str2);
+    stringcomp=false;
+    str1="";
+    str2="";
+    cnt=0;
+  }
+  delay(50);
+  }
+
+
